9637fcbd79a217b37caffba4ca99319b2600ea90
Francin Vincent <francin.vinc@gmail.com>
2022-11-15 14:10:39 +0100

    fix: add scopes again

--- pages/api/auth/[...auth0].ts
+++ pages/api/auth/[...auth0].ts
@@ -2,14 +2,10 @@ import { handleAuth, handleLogin } from "@auth0/nextjs-auth0";
 
 export default handleAuth({
   async login(req, res) {
-    try {
-      await handleLogin(req, res, {
-        authorizationParams: {
-          scope: "openid profile email"
-        }
-      });
-    } catch (error: any) {
-      res.status(error.status ?? 400).end(error.message);
-    }
+    await handleLogin(req, res, {
+      authorizationParams: {
+        scope: "openid profile email read:all"
+      }
+    });
   }
 });
