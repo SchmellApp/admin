439b8aee4cae74dade888ce959dfdaa7fa6c5879
Francin Vincent <francin.vinc@gmail.com>
2022-11-17 14:00:05 +0100

    fix: add audience, update env and add font

--- pages/api/auth/[...auth0].ts
+++ pages/api/auth/[...auth0].ts
@@ -4,7 +4,8 @@ export default handleAuth({
   async login(req, res) {
     await handleLogin(req, res, {
       authorizationParams: {
-        scope: "openid profile email read:schmell"
+        scope: "openid profile email",
+        audience: process.env.NEXT_PUBLIC_AUDIENCE
       }
     });
   }
