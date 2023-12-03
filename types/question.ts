6ad318d1ef4bab427b6325accbb2419b1c5e1887
Francin Vincent <admin@francinvincent.no>
2023-12-03 23:59:15 +0100

    feat: add text group displaying dislike count

--- types/question.ts
+++ types/question.ts
@@ -14,6 +14,7 @@ export interface Question {
   questionPictureUrl?: string;
   questionType: QuestionType;
   groupSize: GroupSize;
+  dislikesCount: number;
 }
 
 export interface QuestionPaginatedResponse {
